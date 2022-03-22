7067077703af9739b0e1db29f77840026844b60e
zenlex <erichrkeil@gmail.com>
2022-03-22 17:25:18 -0500

    switch server to apollo express

--- backend/index.js
+++ backend/index.js
@@ -3,10 +3,15 @@ import dotenv from 'dotenv';
 import jwt from 'jsonwebtoken';
 import mongoose from 'mongoose';
 import User from './models/user.js';
-import { ApolloServer, gql } from 'apollo-server';
+import { ApolloServer } from 'apollo-server-express';
+import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core';
+import { makeExecutableSchema } from '@graphql-tools/schema';
 import resolvers from './resolvers.js';
 import typeDefs from './schema.js';
 import process from 'process';
+import express from 'express';
+import http from 'http';
+
 dotenv.config();
 const { MONGO_URL, JWT_SECRET } = process.env;
 
@@ -15,19 +20,35 @@ mongoose
   .then(() => console.log('Connected to MongoDB'))
   .catch((err) => console.log('error connecting to MongoDB:', err));
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: async ({ req }) => {
-    const auth = req ? req.headers.authorization : null;
-    if (auth && auth.toLowerCase().startsWith('bearer ')) {
-      const decodedToken = await jwt.verify(auth.substring(7), JWT_SECRET);
-      const currentUser = await User.findById(decodedToken.id);
-      return { currentUser };
-    }
-  },
-});
+const start = async () => {
+  const app = express();
+  const httpServer = http.createServer(app);
 
-server.listen().then(({ url }) => {
-  console.log(`Server ready at ${url}`);
-});
+  const schema = makeExecutableSchema({ typeDefs, resolvers });
+  const server = new ApolloServer({
+    schema,
+    context: async ({ req }) => {
+      const auth = req ? req.headers.authorization : null;
+      if (auth && auth.toLowerCase().startsWith('bearer ')) {
+        const decodedToken = await jwt.verify(auth.substring(7), JWT_SECRET);
+        const currentUser = await User.findById(decodedToken.id);
+        return { currentUser };
+      }
+    },
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+  });
+
+  await server.start();
+
+  server.applyMiddleware({
+    app,
+    path: '/',
+  });
+
+  const PORT = process.env.PORT || 4000;
+  httpServer.listen(PORT, () =>
+    console.log(`Server ready at http://localhost:${PORT}`)
+  );
+};
+
+start();
