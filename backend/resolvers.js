0b3a7670edd472d045c72031bbddbad97881a661
zenlex <erichrkeil@gmail.com>
2022-03-22 16:43:46 -0500

    refactor typedefs to schema.js

--- backend/resolvers.js
+++ backend/resolvers.js
@@ -9,59 +9,11 @@ import process from 'process';
 dotenv.config();
 
 const { JWT_SECRET } = process.env;
-
-export const typeDefs = gql`
-  type Query {
-    bookCount: Int!
-    authorCount: Int!
-    allBooks(author: String, genre: String): [Book]!
-    allAuthors: [Author]!
-    me(token: String): User
-  }
-
-  type Token {
-    value: String!
-  }
-
-  type User {
-    username: String!
-    favoriteGenre: String!
-    id: ID!
-  }
-
-  type Book {
-    title: String!
-    author: Author
-    published: Int
-    genres: [String!]!
-    id: ID!
-  }
-
-  type Author {
-    name: String!
-    bookCount: Int
-    born: Int
-    id: ID!
-  }
-
-  type Mutation {
-    addBook(
-      title: String!
-      author: String!
-      published: Int
-      genres: [String]
-    ): Book
-    editAuthor(name: String!, setBornTo: Int): Author
-    createUser(username: String!, favoriteGenre: String!): User
-    login(username: String!, password: String!): Token
-  }
-`;
-
 const countBooks = (author) => {
   author.bookCount = Book.collection.count({ author: author._id });
 };
 
-export const resolvers = {
+const resolvers = {
   Query: {
     bookCount: async (root) => Book.collection.countDocuments(),
     authorCount: (root) => Author.collection.countDocuments(),
@@ -161,3 +113,5 @@ export const resolvers = {
     },
   },
 };
+
+export default resolvers;
---
+++ backend/schema.js
@@ -0,0 +1,50 @@
+import { gql } from 'apollo-server-core';
+
+const typeDefs = gql`
+  type Query {
+    bookCount: Int!
+    authorCount: Int!
+    allBooks(author: String, genre: String): [Book]!
+    allAuthors: [Author]!
+    me(token: String): User
+  }
+
+  type Token {
+    value: String!
+  }
+
+  type User {
+    username: String!
+    favoriteGenre: String!
+    id: ID!
+  }
+
+  type Book {
+    title: String!
+    author: Author
+    published: Int
+    genres: [String!]!
+    id: ID!
+  }
+
+  type Author {
+    name: String!
+    bookCount: Int
+    born: Int
+    id: ID!
+  }
+
+  type Mutation {
+    addBook(
+      title: String!
+      author: String!
+      published: Int
+      genres: [String]
+    ): Book
+    editAuthor(name: String!, setBornTo: Int): Author
+    createUser(username: String!, favoriteGenre: String!): User
+    login(username: String!, password: String!): Token
+  }
+`;
+
+export default typeDefs;
